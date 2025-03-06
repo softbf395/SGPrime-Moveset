8c56f115a57d9bb3166f3d6ca5aa911aa189fd1a
softbf395 <130502864+softbf395@users.noreply.github.com>
2025-03-06 15:11:32 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -13,11 +13,11 @@ function saveFilee(path, content, useURLV)
     end
   end
 end
-function spawnRBXL(URL)
-  return LoadCustomInstance(rbxlURL)
+function spawnRBXL(path)
+  return loadinstance(folder..path)
 end
 saveFilee(songP, songURL, true)
-local PRIME=spawnRBXL(rbxlURL).Rig
+local PRIME=spawnRBXL(rbxlP).Rig
 PRIME.HumanoidRootPart.CFrame=game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 PRIME.Head.title.TextLabel.GUI.Enabled=false
 local chr = game.Players.LocalPlayer.Character
